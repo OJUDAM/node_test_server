d5eebc83ba085a3b9f6dfc8b082f4f4b38f383a4
OJUDAM <dhwnkea@naver.com>
2021-02-03 15:40:30 +0900

    TurnGame TEST2

--- turn.js
+++ turn.js
@@ -9,6 +9,24 @@ var TurnGame=(function(){
             xp: 0,
             att: 10
         };
+        var monsters = [{
+            name: '슬라임',
+            hp: 25 + hero.lev*3,
+            att: 10 + hero.lev, 
+            xp: 10 + hero.lev  
+        },{
+            name: '스켈레톤',
+            hp: 50 + hero.lev*5,
+            att: 15 + hero.lev*2,
+            xp: 20 + hero.lev*2
+        },{
+            name: '마왕[보스]',
+            hp: 100 + hero.lev*10,
+            att: 25 + hero.lev*5,
+            xp: 50 + hero.lev*5
+        }];
+        var monster = null;
+        var turn = true;
         return {
             showLevel: function(){
                 document.getElementById('hero-level').innerHTML = hero.lev + 'lev'
@@ -54,6 +72,94 @@ var TurnGame=(function(){
             setMessage: function(msg) {
                 document.getElementById('message').innerHTML = msg;
                 return this;
+            },
+            generateMonster: function(){
+                monster = JSON.parse(JSON.stringify(monsters[Math.floor(Math.random() * monsters.length)]));
+                document.getElementById('monster-name').innerHTML = monster.name;
+                document.getElementById('monster-hp').innerHTML = 'HP: ' + monster.hp;
+                document.getElementById('monster-att').innerHTML = 'ATT: ' + monster.att;
+                this.setMessage(monster.name + '이(가) 공격해옵니다');
+                return this.toggleMenu();
+            },
+            menuInput: function(input){
+                if(input === '1'){
+                    return this.generateMonster();
+                }else if(input === '2'){
+                    hero.hp = hero.maxHp;
+                    return this.showHp().setMessage('체력을 회복했습니다.');
+                }else if(input === '3'){
+                    return this.exit();
+                }else{
+                    alert('잘못된 입력');
+                }
+            },
+            battleInput: function(input){
+                if(input === '1'){
+                    return this.attackMonster();
+                }else if(input === '2'){
+                    if(hero.hp + hero.lev * 20 < hero.maxHp){
+                        hero.hp += hero.lev * 20;
+                    }else{
+                        hero.hp = hero.maxHp;
+                    }
+                    return this.showHp().setMessage('체력을 회복했습니다.').nextTurn();
+                }else if( input === '3'){
+                    return this.clearMonster().setMessage('도망쳤습니다.');
+                }else{
+                    alert('잘못된 입력')
+                }
+            },
+            attackMonster: function(){
+                monster.hp -= hero.att;
+                document.getElementById('monster-hp').innerHTML = 'HP: ' + monster.hp;
+                if(monster.hp > 0){
+                    return this.setMessage(hero.att + '의 데이지를 입혔습니다.').nextTurn();
+                }
+                return this.win();
+            },
+            attackHero: function(){
+                hero.hp -= monster.att;
+                return this.showHp();
+            },
+            nextTurn: function(){
+                var self = this;
+                turn = !turn;
+                document.getElementById('battle-button').disabled = true;
+                if(!turn){
+                    window.setTimeout(function(){
+                        self.setMessage(monster.name+ '의 턴입니다.');
+                        window.setTimeout(function(){
+                            document.getElementById('battle-button').disabled = false;
+                            if(self.attackHero()){
+                                self.setMessage(monster.att + '의 데미지를 입었습니다.');
+                                window.setTimeout(function(){
+                                    self.setMessage(hero.name + '의 턴입니다.');
+                                }, 1000);
+                            }
+                        }, 1000);
+                    }, 1000);
+                    return this.nextTurn();
+                }
+                return this;
+            },
+            win: function(){
+                this.setMessage(monster.name + '사냥에 성공해 경험치 ' + monster.xp+'을 얻었습니다!');
+                hero.xp += monster.xp;
+                return this.clearMonster().showXp();
+            },
+            clearMonster: function(){
+                monster = null;
+                document.getElementById('monster-name').innerHTML = '';
+                document.getElementById('monster-hp').innerHTML = '';
+                document.getElementById('monster-att').innerHTML = '';
+                return this.toggleMenu();
+            },
+            gameOver: function(){
+                document.getElementById('screen').innerHTML = hero+'은 레벨'+hero.lev + '에서 죽었습니다. 새로 시작하려면 새로고침을 눌러주세요';
+                return false;
+            },
+            exit: function(input){
+                document.getElementById('screen').innerHTML = '이용해주셔서 감사합니다.'
             }
         };
     };
@@ -80,10 +186,12 @@ document.getElementById('game-menu').onsubmit = function(e){
     var option = input.value;
     e.preventDefault();
     input.value = '';
+    TurnGame.getInstance().menuInput(option);
 };
 document.getElementById('battle-menu').onsubmit = function(e){
     var input = document.getElementById('battle-input');
     var option = input.value;
     e.preventDefault();
     input.value = '';
+    TurnGame.getInstance().battleInput(option);
 };
